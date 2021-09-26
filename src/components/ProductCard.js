9104e973588d98a29896a88ad9439147bb02e90c
Siegfred <dante_sparda1925@yahoo.com>
2021-09-26 08:22:26 +0300

    changed the product card component

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,8 +6,11 @@ const ProductCard = () => {
             <img src={Image3} className="card-img" alt="..."/>
             <div className="ps-3">
                 <div className="fw-bold fs-6">Product Name</div>
-                <div>Lorem ipsum lorem</div>
-                <div>Price:20</div>
+                <div className="card-item">Lorem ipsum lorem</div>
+                <div className="card-item">Price:20</div>
+            </div>
+             <div className="text-center">
+                <button className="btn btn-primary btn-sm">add to cart</button>
             </div>
         </div>
     )
