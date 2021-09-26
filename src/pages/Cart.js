31ac3aef5c41fac366352ae982ff8dfab9c2f0bb
Siegfred <dante_sparda1925@yahoo.com>
2021-09-26 16:49:39 +0300

    updated the button links

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -12,7 +12,8 @@ const Cart = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-6 col-md-8 col-10 mx-auto">
+                    <div className="row">
+                        <div className="col-lg-6 col-md-8 col-10 mx-auto">
                             <div className="cart-buttons">
                                 <div className="cart-links">
                                 <button className="btn btn-primary col-12 mx-auto fs-6 mb-2" type="submit">Login</button>
@@ -20,6 +21,7 @@ const Cart = () => {
                                 </div>
                             </div>
                         </div>
+                    </div>
                 </div>
             </div>
         </div>
