067949f140c94534cccc18b1a982cb4d2e3146d4
Siegfred <dante_sparda1925@yahoo.com>
2021-09-26 16:45:46 +0300

    added the cart component

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,7 +3,25 @@ import React from 'react'
 const Cart = () => {
     return (
         <div className="cart-page">
-            <h1 >Cart Page</h1> 
+            <div className="container">
+                <div className="row"> 
+                    <div className="col">
+                        <div className="cart-message">
+                            <div className="heading p-5">
+                                <h3 className="text-center text-light">There are no products in your shopping cart.</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 col-md-8 col-10 mx-auto">
+                            <div className="cart-buttons">
+                                <div className="cart-links">
+                                <button className="btn btn-primary col-12 mx-auto fs-6 mb-2" type="submit">Login</button>
+                                <button className="btn btn-warning col-12 mx-auto fs-6" type="submit">Continue shopping</button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
         </div>
     )
 }
