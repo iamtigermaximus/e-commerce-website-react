2a7cd1fe9ae224cb160a2e694492bd429676ab45
Siegfred <dante_sparda1925@yahoo.com>
2021-09-26 13:15:30 +0300

    changed the card properties(size and font)

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,15 +2,17 @@ import React from 'react'
 
 const ProductCard = (props) => {
     return (
-        <div className="card">
-            <img src={props.image} className="card-img" alt="..."/>
-            <div className="ps-3">
-                <div className="fw-bold fs-6">{props.name}</div>
+        <div className="card-container">
+            <div className="card-image-container">
+                <img src={props.image} className="card-image" alt="..."/>
+            </div>
+            <div className="ps-3 card--body">
+                <div className="card-name">{props.name}</div>
                 <div className="card-item">{props.description}</div>
                 <div className="card-item">Price:{props.price}</div>
             </div>
              <div className="text-center">
-                <button className="btn btn-primary btn-sm">add to cart</button>
+                <button className="btn btn-primary"><div className="card-item">add to cart</div></button>
             </div>
         </div>
     )
